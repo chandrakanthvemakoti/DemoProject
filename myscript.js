b1ea7c1188fafbc6259bcc7c618c404742fa22d8
chandrakanthvemakoti <chanduvemakoti@gmail.com>
2020-05-07 17:49:21 +0530

    login updated

--- myscript.js
+++ myscript.js
@@ -2,23 +2,25 @@ function inputValidation(val){
     if (val =="username"){
         var x=document.getElementById("username").value;
         if(x==""){
-            document.getElementById("usernameError").style.display="block";
+            document.getElementById("usernameError").style.visibility="visible";
 
         }
         else{
-            document.getElementById("usernameError").style.display="none";
+            document.getElementById("usernameError").style.visibility="hidden";
         }
+       
     }
 
     if(val== "password"){
         var y=document.getElementById("password").value;
         if(y==""){
-            document.getElementById("passwordError").style.display="block";
+            document.getElementById("passwordError").style.visibility="visible";
 
         }
         else{
-            document.getElementById("passwordError").style.display="none"
+            document.getElementById("passwordError").style.visibility="hidden";
         }
+        
     }
 
 
@@ -30,17 +32,21 @@ function validation(){
     
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    var forms = document.getElementById("forms").addEventListener("click",function(event){
-        event.preventDefault();
-    });
+    // var  submit =  document.getElementById("submit").addEventListener("click",function(event){
+    //     event.preventDefault()
+    // });
     
 
     if(username== null || username==""){
-    document.getElementById("usernameError").style.display="block";
+    document.getElementById("usernameError").style.visibility="visible";
+    return false;
     }
+    
+    
     if(password == null || password=="")
     {
-    document.getElementById("passwordError").style.display="block";
+    document.getElementById("passwordError").style.visibility="visible";
+    return false;
     }
   
     
